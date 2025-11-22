076f66bdeadb11a5030d4fda62a89ff44102c705
Victor Bessonov <victor.a.bessonov@gmail.com>
2025-11-22 18:13:27 +0300

    Tweak packages

--- package/custom/gl-e750-mcu/files/luci/control/mcu_interface.lua
+++
@@ -1,5 +0,0 @@
-module("luci.controller.admin.mcu_interface", package.seeall)
- 
-function index()
-    entry({"admin", "system", "MCU-interface"}, cbi("admin_system/mcu_interface"), _("MCU Interface"), 1)
-end
